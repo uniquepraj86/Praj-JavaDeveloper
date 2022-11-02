0c18e81ac70b0a49c20f79aadf648c172c7a736d
prajkta <prajktaarote86@gmail.com>
2022-11-02 10:47:24 +0530

    Praj-Star Pattern

---
+++ src/starpattern/RightAngleTriangle.java
@@ -0,0 +1,13 @@
+package starpattern;
+
+public class RightAngleTriangle {
+    public static void main(String[] args) {
+    int i,j,k = 6;
+    for(i=0;i<k;i++){
+        for(j=0;j<=i;j++){
+            System.out.print("P ");
+        }
+        System.out.println();
+    }
+    }
+}
